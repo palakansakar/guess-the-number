7e511f8c7eb96296e6ccb4c0349d5f56e8e1a020
Pala <palakansakar@gmail.com>
2016-05-13 03:21:04 -0600

    Number Guessing Assignment Committed

---
+++ Slidify Pitch of Guess The Number/codes.R
@@ -0,0 +1,10 @@
+#install and load devtools
+install.packages("devtools")
+library(devtools)
+
+#install and load Slidify
+install_github('slidify', 'pala')
+install_github('slidifyLibraries', 'pala')
+library(slidify)
+
+author("Guess_The_Number")
